c69d0fab91641cf4e9f44aa44391f157f601f351
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2023-03-26 17:29:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function delayMessage() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+const outputDiv = document.getElementById('output');
+
+delayMessage().then((message) => {
+  outputDiv.innerHTML = message;
+});
